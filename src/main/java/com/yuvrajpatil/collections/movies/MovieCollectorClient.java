676d321b6def622274acef0bcec18f0020d40318
Yuvraj Patil <yuvraj.patil.it@gmail.com>
2017-03-05 15:26:41 +0530

    EXPORT_DIRECTORIES
    1. Changed paths as per linux.
    2. Imported EXPORT_DIRECTORIES from JSON array.
    3. Added "IMDb.zip" in IMDB check.
    4. Removed unused imports

--- src/main/java/com/yuvrajpatil/collections/movies/MovieCollectorClient.java
+++ src/main/java/com/yuvrajpatil/collections/movies/MovieCollectorClient.java
@@ -5,26 +5,21 @@ package com.yuvrajpatil.collections.movies;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.yuvrajpatil.movies.beans.Movie;
 
 import java.io.FileReader;
-import java.util.Iterator;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 
-
 /**
- * @author Yuvraj.Patil
- *         TODO: change 'folder' to 'directory'
- *         TODO: Add a file logger instead of System.log
- *         TODO: Sort movies in a series as per year
+ * @author Yuvraj.Patil TODO: change 'folder' to 'directory' TODO: Add a file
+ * logger instead of System.log TODO: Sort movies in a series as per year
  */
 /**
  * @author Yuvraj.Patil
@@ -32,52 +27,59 @@ import org.json.simple.parser.JSONParser;
  */
 class MovieCollectorClient {
 
-     final static String ROOT_DIRECTORY;     
-     final static String DIRECTORY_SEPARATOR;
-     final static String LANG_DIRECTORY;
-     final static String EXPORT_DIRECTORY;
-     
-     static {
-    	 JSONParser parser = new JSONParser();
-    	 JSONObject jsonObject = null;
-    	 
-    	 try {
-    		 
-             Object obj = parser.parse(new FileReader(
-                     "config.json"));
-  
-             jsonObject = (JSONObject) obj;
-             
-    	 }catch(Exception e) {
-    		 System.out.println(e);
-    	 }
-    	 
-    	 ROOT_DIRECTORY = (String) jsonObject.get("ROOT_DIRECTORY");     
-         DIRECTORY_SEPARATOR = (String) jsonObject.get("DIRECTORY_SEPARATOR");
-         LANG_DIRECTORY = (String) jsonObject.get("LANG_DIRECTORY");
-         EXPORT_DIRECTORY = (String) jsonObject.get("EXPORT_DIRECTORY") + 
-        		 								LANG_DIRECTORY + DIRECTORY_SEPARATOR;
-    	 
-    	 
-     }
+    final static String ROOT_DIRECTORY;
+    final static String DIRECTORY_SEPARATOR;
+    final static String LANG_DIRECTORY;
+    final static String EXPORT_DIRECTORIES[];
+
+    static {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = null;
+
+        try {
+
+            Object obj = parser.parse(new FileReader(
+                    "config.json"));
+
+            jsonObject = (JSONObject) obj;
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        ROOT_DIRECTORY = (String) jsonObject.get("ROOT_DIRECTORY");
+        //  DIRECTORY_SEPARATOR = (String) jsonObject.get("DIRECTORY_SEPARATOR");
+        DIRECTORY_SEPARATOR = System.getProperties().get("file.separator").toString();
+        LANG_DIRECTORY = (String) jsonObject.get("LANG_DIRECTORY");
+
+        JSONArray exportDirectories = (JSONArray) jsonObject.get("EXPORT_DIRECTORIES");
+
+        EXPORT_DIRECTORIES = new String[exportDirectories.size()];
+
+        for (int i = 0; i < exportDirectories.size(); i++) {
+            EXPORT_DIRECTORIES[i] = (String) exportDirectories.get(i);
+        }
+
+    }
+
     /**
      * @param args
      */
     public static void main(String[] args) {
-        try{
-        	final File folder = new File(ROOT_DIRECTORY + DIRECTORY_SEPARATOR + LANG_DIRECTORY);
-	        for (final File fileEntry : folder.listFiles()) {
-	            printDirectories(ROOT_DIRECTORY + DIRECTORY_SEPARATOR + LANG_DIRECTORY,
-	            		fileEntry.getName());
-	        }
-	        log("Movies Collection Analysis Successful");
-        }catch(Exception e) {
-        	log("ERROR: " + e);
+        try {
+            final File folder = new File(ROOT_DIRECTORY + DIRECTORY_SEPARATOR + LANG_DIRECTORY);
+            for (final File fileEntry : folder.listFiles()) {
+                printDirectories(ROOT_DIRECTORY + DIRECTORY_SEPARATOR + LANG_DIRECTORY,
+                        fileEntry.getName());
+            }
+            log("Movies Collection Analysis Successful");
+        } catch (Exception e) {
+            log("ERROR: " + e);
         }
     }
 
     private static void printDirectories(String rootDirectory, String category) {
-        final File folder = new File(rootDirectory + "//" + category);
+        final File folder = new File(rootDirectory + DIRECTORY_SEPARATOR + category);
 
         List<Movie> movieCollection = new ArrayList<Movie>();
         List<Movie> tmpSeries = new ArrayList<Movie>();
@@ -85,69 +87,73 @@ class MovieCollectorClient {
         for (final File fileEntry : folder.listFiles()) {
             try {
                 tmpSeries = getSeries(fileEntry);
-            }catch (Exception e){
+            } catch (Exception e) {
                 log("ERROR in File name" + e + fileEntry.getName());
             }
             movieCollection.addAll(tmpSeries);
         }
-        exportCollection(movieCollection, category);        
+
+        for (String exportDirectory : EXPORT_DIRECTORIES) {
+            exportCollection(movieCollection, exportDirectory, category);
+        }
+
     }
 
     private static List<Movie> getSeries(File folder) {
-      //  Series movieSeries = new Series();
+        //  Series movieSeries = new Series();
 
         List<Movie> movies = new ArrayList<Movie>();
 
         Movie tmpMovie = new Movie();
         String folderName = folder.getName();
-        
+
         if (!isSeries(folderName)) {		// If it is a movie ( Non-series )
-        	boolean isIMDBPagePresent = false;
+            boolean isIMDBPagePresent = false;
             for (final File fileEntry : folder.listFiles()) {
-            	String fileName = fileEntry.getName(); 
-            	if(fileName.contains("IMDb.rar")) {
-            		isIMDBPagePresent = true;
-            		continue;
-            	}
-                if(isValidFile(fileName)){
-                        tmpMovie = getRefactoredMovie(new Movie(folderName, fileEntry.getName()));
-                        tmpMovie.setSeriesName("");
-                        movies.add(tmpMovie);                        
+                String fileName = fileEntry.getName();
+                if (fileName.contains("IMDb.rar") || fileName.contains("IMDb.zip")) {
+                    isIMDBPagePresent = true;
+                    continue;
+                }
+                if (isValidFile(fileName)) {
+                    tmpMovie = getRefactoredMovie(new Movie(folderName, fileEntry.getName()));
+                    tmpMovie.setSeriesName("");
+                    movies.add(tmpMovie);
                 }
             }
 
-            if(!isIMDBPagePresent) {
-            	warn("IMDB information not present in directory : " + folderName);
+            if (!isIMDBPagePresent) {
+                warn("IMDB information not present in directory : " + folderName);
             }
             return movies;
-        }else{						// If it is a series
-        	
+        } else {						// If it is a series
+
             for (final File seriesFolder : folder.listFiles()) {
-            	
-                if(!seriesFolder.isDirectory()){
-                    if(seriesFolder.getName().equals("Series.rar") || seriesFolder.getName().equals("Thumbs.db") ){
+
+                if (!seriesFolder.isDirectory()) {
+                    if (seriesFolder.getName().equals("Series.rar") || seriesFolder.getName().equals("Thumbs.db")) {
                         continue;
                     }
-                    logOut("Naming standards are not followed by series : " + seriesFolder.getName() + 
-                    		"\n\t\t Only movie folders and \"Series.rar\" is allowed inside a movie series folder");
+                    logOut("Naming standards are not followed by series : " + seriesFolder.getName()
+                            + "\n\t\t Only movie folders and \"Series.rar\" is allowed inside a movie series folder");
                 }
                 boolean isIMDBPagePresent = false;
                 for (final File movieFolder : seriesFolder.listFiles()) {
-            		// Movies of a series
-                	
-                	String fileName = movieFolder.getName();
-                	if(fileName.contains("IMDb.rar")) {
-                		isIMDBPagePresent = true;
-                		continue;
-                	}
-                    if(isValidFile(movieFolder.getName())){
+                    // Movies of a series
+
+                    String fileName = movieFolder.getName();
+                    if (fileName.contains("IMDb.rar") || fileName.contains("IMDb.zip")) {
+                        isIMDBPagePresent = true;
+                        continue;
+                    }
+                    if (isValidFile(movieFolder.getName())) {
                         tmpMovie = getRefactoredMovie(new Movie(seriesFolder.getName(), movieFolder.getName()));
                         tmpMovie.setSeriesName(folderName);
                         movies.add(tmpMovie);
                     }
                 }
-                if(!isIMDBPagePresent) {
-                	warn("IMDB information not present in directory : " + seriesFolder);
+                if (!isIMDBPagePresent) {
+                    warn("IMDB information not present in directory : " + seriesFolder);
                 }
             }
         }
@@ -156,17 +162,22 @@ class MovieCollectorClient {
 
     }
 
-    private static void exportCollection(List<Movie> collection, String category) {
-        try{
-            File file = new File( EXPORT_DIRECTORY + category + ".json");
-            if(!file.exists()){
+    private static void exportCollection(List<Movie> collection, String exportDirectory, String category) {
+        try {
+            File directory = new File(exportDirectory + LANG_DIRECTORY);
+
+            directory.mkdir();  //  create a new directory, will do nothing if directory exists
+
+            File file = new File(directory, category + ".json");
+
+            if (!file.exists()) {
                 file.createNewFile();
             }
             FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
             BufferedWriter buffWriter = new BufferedWriter(fileWriter);
             buffWriter.write(collection.toString());
             buffWriter.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             log("ERROR in exporting data : " + e);
         }
     }
@@ -177,19 +188,19 @@ class MovieCollectorClient {
         int endIndex = movieName.indexOf(")");
 
         if (startIndex == -1 || endIndex == -1 || startIndex == 0 || endIndex == 0) {
-        	log("Naming standards are not followed by: " + movieName);
+            log("Naming standards are not followed by: " + movieName);
             logOut("Year of release is not specified as per standards");
-            
+
         }
         String fileName = movie.getFileName().toLowerCase();
 
-        if(fileName.contains("hindi") ) {
-        	movie.setHindi(true);
+        if (fileName.contains("hindi")) {
+            movie.setHindi(true);
         }
         assignPrintQuality(movie, movie.getFileName());
 
         try {
-            movie.setName(movieName.substring(0, startIndex-1));
+            movie.setName(movieName.substring(0, startIndex - 1));
             movie.setYear(Integer.parseInt(movieName.substring(startIndex + 1,
                     endIndex)));
         } catch (IndexOutOfBoundsException exception) {
@@ -199,27 +210,26 @@ class MovieCollectorClient {
         }
         return movie;
     }
-    
+
     /**
      * @description Function to assign print quality from File Name
      * @param movie	Movie object
      * @param fileName	File name
      */
-    
-    private static void assignPrintQuality (Movie movie, String fileName) {
-    	
-    	if(fileName.contains("1080p")) {
-        	movie.setPrintQuality("1080p");
-        }else if(fileName.contains("720p")) {
-        	movie.setPrintQuality("720p");
-        }else if(fileName.contains("TC")) {
-        	movie.setPrintQuality("TC");
-        }else if(fileName.contains("480p")) {
-        	movie.setPrintQuality("480p");
-        }else if(fileName.contains("DVD") || fileName.contains("dvd") ) {
-        	movie.setPrintQuality("DVD");
-        }else {
-        	movie.setPrintQuality("");
+    private static void assignPrintQuality(Movie movie, String fileName) {
+
+        if (fileName.contains("1080p")) {
+            movie.setPrintQuality("1080p");
+        } else if (fileName.contains("720p")) {
+            movie.setPrintQuality("720p");
+        } else if (fileName.contains("TC")) {
+            movie.setPrintQuality("TC");
+        } else if (fileName.contains("480p")) {
+            movie.setPrintQuality("480p");
+        } else if (fileName.contains("DVD") || fileName.contains("dvd")) {
+            movie.setPrintQuality("DVD");
+        } else {
+            movie.setPrintQuality("");
         }
     }
 
@@ -231,24 +241,27 @@ class MovieCollectorClient {
     }
 
     private static boolean isValidFile(String fileName) {
-        if(fileName.contains(".srt") || 
-        		fileName.contains(".rar") || 
-        		fileName.contains(".db") || 
-        		fileName.contains(".txt") || 
-        		fileName.contains(".jpg")|| 
-        		fileName.contains(".png")){
-           return false;
+        if (fileName.contains(".srt")
+                || fileName.contains(".rar")
+                || fileName.contains(".db")
+                || fileName.contains(".txt")
+                || fileName.contains(".jpg")
+                || fileName.contains(".png")) {
+            return false;
         }
         return true;
     }
-    private static void logOut(Object msg){
+
+    private static void logOut(Object msg) {
         System.out.println("\nFATAL ERROR: " + msg);
-        System.exit(0);        
+        System.exit(0);
     }
-    private static void warn(Object msg){
+
+    private static void warn(Object msg) {
         System.out.println("WARNING: " + msg);
     }
-    private static void log(Object msg){
+
+    private static void log(Object msg) {
         System.out.println("INFO: " + msg);
     }
 }
